c2be07be740f0ae29caf4d185602c7938c6a1211
Miles King <kingmilest@gmail.comgit config --global user.name Miles>
2026-01-21 21:24:39 -0500

    Added move validation to graphical board

--- scripts/TrainingManager.js
+++ scripts/TrainingManager.js
@@ -1,20 +1,46 @@
 import {Chess} from 'chess.js'
 import '../node_modules/@chrisoakman/chessboard2/dist/chessboard2.min.js'
+
 function TrainingManager(boardElementId = "trainingBoard") {
-    const gameLogic = new Chess()
-    const chessBoard = Chessboard2(boardElementId, 'start')
+    const logicalBoard = new Chess()
+    let boardState
+    const config = {
+        position: 'start',
+        draggable: true,
+        onDrop,
+        onDragStart: startMove,
+    }
+    const graphicalBoard = Chessboard2(boardElementId, config)
 
     /**
      * Initializes chess state and UI chess board
      */
     function prepareForTraining() {
-        chessBoard.start()
-        gameLogic.reset()
+        graphicalBoard.start()
+        logicalBoard.reset()
     }
     function getFen() {
-        return gameLogic.fen()
+        return logicalBoard.fen()
     }
     return {prepareForTraining, getFen}
+
+    /**
+     * Fires when piece is dropped on chessboard. Returning 'snapback' to chessBoard2 reverts drop.
+     * @param {Object} pieceMoved - The chessBoard library representation of piece object that was dropped
+     */
+    function onDrop (pieceMoved) {
+        try {
+            logicalBoard.move({from: pieceMoved.source, to: pieceMoved.target})
+        }
+        catch (e) {
+            console.error(e)
+            return 'snapback'
+        }
+    }
+
+    function startMove(){
+        boardState = logicalBoard.fen()
+    }
 }
 
 export {TrainingManager, Chess}
---
+++ scripts/pages/TrainingPage.js
@@ -0,0 +1,5 @@
+import '../../node_modules/@chrisoakman/chessboard2/dist/chessboard2.css'
+import {TrainingManager} from '../TrainingManager.js'
+
+TrainingManager("trainingBoard")
+
