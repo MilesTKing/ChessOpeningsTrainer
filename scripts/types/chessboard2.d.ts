35dd691b8324c9095550c16b05035af9957aa767
Miles King <kingmilest@gmail.com>
2026-01-30 18:48:35 -0500

    Added Chessboard2 library types declaration file.

--- scripts/types/chessboard2.d.ts
+++ scripts/types/chessboard2.d.ts
@@ -1,15 +1,24 @@
-// declare function Chessboard2(
-//     elementId: string,
-//     config: {
-//         position?: string
-//         draggable?: boolean
-//         onDrop?: (piece: any) => 'snapback' | void
-//         onDragStart?: (piece: any) => void
-//     }
-// ): {
-//     start(): void
-//     position(fen?: string, animate?: boolean): void
-// }
-//
-// export {}
-declare module "Chessboard2";
+declare function Chessboard2(
+    elementId: string | HTMLElement,
+    config?: Chessboard2Config
+): Chessboard2Instance
+
+declare interface Chessboard2Config {
+    position?: string
+    draggable?: boolean
+    orientation?: 'white' | 'black'
+    onDrop?: (move: {
+        source: string
+        target: string
+        piece: string
+    }) => void
+    onDragStart?: (...args: any[]) => void
+}
+
+declare interface Chessboard2Instance {
+    start(): void
+    clear(): void
+    position(fen?: string, animate?: boolean): string | void
+    orientation(): 'white' | 'black'
+    destroy(): void
+}
