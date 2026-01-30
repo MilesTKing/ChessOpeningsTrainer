b6e31dcc2a12626c5f555cffe3d67c14c78547b7
Miles King <kingmilest@gmail.com>
2026-01-30 12:22:42 -0500

    Added chess playerColor type

--- scripts/PathwayCustomizer.ts
+++ scripts/PathwayCustomizer.ts
@@ -2,29 +2,30 @@ import {Chess} from 'chess.js'
 import '../node_modules/@chrisoakman/chessboard2/dist/chessboard2.min.js'
 
 function PathwayCustomizer(){
+    type playerColor = 'w' | 'b'
+    interface Node {
+        fen: string;
+        nextPositions: Map<string, Node>;
+    }
     const logicalBoard = new Chess();
     const graphicalBoard = Chessboard2(boardElementId, config);
-    let userColor;
+    let userColor: playerColor;
+
     const config = {
         position: 'start',
         draggable: true,
         onDrop,
     }
-
-    interface Node {
-        fen: string;
-        nextPositions: Map<string, Node>;
-    }
     function createNode(fen: string): Node{
         let nextPositions: Map<string, Node> = new Map();
         return {fen, nextPositions}
     }
-    function addPossibleMove(currentMove: Node, moveToAdd: string){
+    function addPossibleMove(currentPosition: Node, moveToAdd: string){
         const newMoveNode = createNode(moveToAdd)
-        currentMove.nextPositions.set(moveToAdd, newMoveNode);
+        currentPosition.nextPositions.set(moveToAdd, newMoveNode);
     }
-    function removePossibleMove(currentMove: Node, moveToRemove: string){
-        currentMove.nextPositions.delete(moveToRemove);
+    function removePossibleMove(currentPosition: Node, moveToRemove: string){
+        currentPosition.nextPositions.delete(moveToRemove);
     }
 
 
