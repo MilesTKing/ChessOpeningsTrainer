5e573e48e1490b0716a47a0755977ee460c53c8c
Miles King <kingmilest@gmail.com>
2026-01-31 23:21:07 -0500

    Added Chessboard2 method descriptions and all documented methods.

--- scripts/types/chessboard2.d.ts
+++ scripts/types/chessboard2.d.ts
@@ -28,9 +28,40 @@ declare interface ChessboardDropEvent {
 }
 
 declare interface Chessboard2Instance {
+    /** Sets the board to the start position.
+
+     If useAnimation is false, sets the position instantly.**/
     start(): void
+
+    /** Removes all the pieces on the board.
+
+     If useAnimation is false, removes pieces instantly.**/
     clear(): void
-    position(fen?: string, animate?: boolean): string | void
-    orientation(): 'white' | 'black'
+
+    /** Remove the widget from the DOM. **/
     destroy(): void
+
+    /** Returns the current position as a Position Object.
+
+     If the first argument is 'fen', returns the position as a FEN string.**/
+    position(returnType?: 'fen')
+
+    /** Animates to a new position.
+
+     If useAnimation is false, sets the position instantly.**/
+    position(fen?: PositionObject | 'fen' | 'start', useAnimation?: boolean): string
+
+    orientation(): 'white' | 'black'
+    orientation(side: 'white' | 'black' | 'flip'): void
+
+    /** Returns the current position as a FEN string. **/
+    fen(): string
+
+    flip(): void
+    resize(): void
+    move(move: string | string[]): void
+}
+
+declare interface PositionObject {
+
 }
