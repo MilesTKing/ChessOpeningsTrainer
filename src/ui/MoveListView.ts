f0deaa6c7a79431bbc0453f706bd9f20feb5fd4f
Miles King <kingmilest@gmail.com>
2026-03-03 21:37:47 -0500

    Make nodes non-draggable. Added right click menu to nodes.

--- src/ui/MoveListView.ts
+++ src/ui/MoveListView.ts
@@ -1,6 +1,10 @@
 import cytoscape from "cytoscape";
 import dagre from "cytoscape-dagre";
+import contextMenus from 'cytoscape-context-menus';
+
 cytoscape.use(dagre);
+cytoscape.use(contextMenus);
+import 'cytoscape-context-menus/cytoscape-context-menus.css';
 
 type nodeSelectionHandler = (nodeId: number) => void;
 
@@ -31,6 +35,7 @@ function MoveListView(opts?: { headless?: boolean }) {
                         'background-width': '1.2em',
                         'background-height': '1.2em',
                         'background-position-x': '0',
+                        'draggable': 'false',
                         // @ts-ignore
                         'text-margin-x': '.5em'
                     }
@@ -45,10 +50,50 @@ function MoveListView(opts?: { headless?: boolean }) {
                         'target-arrow-shape': 'triangle',
                         'curve-style': 'bezier'
                     }
-                }
+                },
             ],
+        autoungrabify: true
 
+    })
+    const context_menu_options = {
+        // Customize event to bring up the context menu
+        // Possible options https://js.cytoscape.org/#events/user-input-device-events
+        evtType: 'cxttap',
+        // List of initial menu items
+        // A menu item must have either onClickFunction or submenu or both
+        menuItems: [
 
+      {
+        id: 'remove', // ID of menu item
+        content: 'remove', // Display content of menu item
+        tooltipText: 'remove', // Tooltip text for menu item
+        // Filters the elements to have this menu item on cxttap
+        // If the selector is not truthy no elements will have this menu item on cxttap
+        selector: 'node',
+        onClickFunction: function () { // The function to be executed on click
+          console.log('remove element');
+        },
+        disabled: false, // Whether the item will be created as disabled
+        show: true, // Whether the item will be shown or not
+        hasTrailingDivider: true, // Whether the item will have a trailing divider
+        coreAsWell: false, // Whether core instance have this item on cxttap
+        submenu: [] // Shows the listed menuItems as a submenu for this item. An item must have either submenu or onClickFunction or both.
+      },
+        ],
+        // css classes that menu items will have
+        menuItemClasses: [
+            // add class names to this list
+        ],
+        // css classes that context menu will have
+        contextMenuClasses: [
+            // add class names to this list
+        ],
+        // Indicates that the menu item has a submenu. If not provided default one will be used
+        submenuIndicator: { src: 'assets/submenu-indicator-default.svg', width: 12, height: 12 }
+    };
+    const contextMenuInstance = moveListGraph.contextMenus(context_menu_options);
+    moveListGraph.on('cxttap', (event: any) => {
+        console.log(contextMenuInstance.isActive())
     })
     function runTreeLayout() {
         moveListGraph.layout({
