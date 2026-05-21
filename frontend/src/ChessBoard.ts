1587b232d4f87a5445a9ee29a23d3ac796079a8e
Miles King <kingmilest@gmail.comgit config --global user.name Miles>
2026-05-20 21:08:03 -0400

    Updated config to path to chessboard piece images.

--- frontend/src/ChessBoard.ts
+++ frontend/src/ChessBoard.ts
@@ -1,14 +1,15 @@
 import '../node_modules/@chrisoakman/chessboardjs/dist/chessboard-1.0.0.min.js'
 
-function ChessBoard(boardElementId: string, onDrop) {
+function ChessBoard(boardElementId: string, onDrop, onMoveEnd?) {
     const config= {
         position: 'start',
         draggable: true,
         onDrop,
+        onMoveEnd,
+        pieceTheme: 'src/assets/{piece}.svg',
         showErrors: 'console'
     }
     const chessboard = Chessboard(boardElementId, config);
-    console.log(chessboard);
     function setPosition(position: string) {
         chessboard.position(position)
     }
