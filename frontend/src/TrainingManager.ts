e6d725cdfbfabeb24c6f92de9c9092828e09a81b
Miles King <kingmilest@gmail.comgit config --global user.name Miles>
2026-05-22 07:28:54 -0400

    Training manager filters out last moves of pathways.

--- frontend/src/TrainingManager.ts
+++ frontend/src/TrainingManager.ts
@@ -1,16 +1,17 @@
 import {Chess} from 'chess.js'
 import {getCookie} from './utils/cookies'
+
 function TrainingManager() {
     const API_BASE_URL = import.meta.env.VITE_OPENINGS_API_BASE_URL
     const OPENINGS_URL = import.meta.env.VITE_OPENINGS_API_OPENINGS
     const chessBoard = new Chess()
     let attemptedMoves = 0
-    let correctMoves= 0
+    let correctMoves = 0
     let accuracy = 0
-    let trainingPositions: SerializedPathNode[]
     let pathPositionMap: Map<number, SerializedPathNode> = new Map()
     let activePositionId = 0
     let trainingPositionIds: number[] = []
+
     async function getUserPathways(source: 'api' | 'localStorage') {
         if (source === 'api') {
             console.log('using api method')
@@ -40,10 +41,9 @@ function TrainingManager() {
     function setupTraining(selectedPathwayName: string, selectedPathwayPositions: SerializedPathNode[]) {
         chessBoard.reset()
         attemptedMoves = 0
-        correctMoves= 0
+        correctMoves = 0
         accuracy = 0
-        trainingPositions = selectedPathwayPositions
-        for (const node of trainingPositions) {
+        for (const node of selectedPathwayPositions) {
             pathPositionMap.set(node.id, node)
         }
     }
@@ -51,17 +51,28 @@ function TrainingManager() {
     function getBoardPosition() {
         return chessBoard.fen({forceEnpassantSquare: true})
     }
-    function startOpeningTest(){
+
+    function startOpeningTest() {
         chessBoard.reset()
         accuracy = 0
         trainingPositionIds = Array.from(pathPositionMap.keys())
-        if(!trainingPositionIds){
+        if (!trainingPositionIds) {
             throw new Error("No Training Pathway selected.")
         }
-        trainingPositionIds.pop() //Removes last position because there's no next moves.
+        const endMoves: number[] = []
+        for (const id in trainingPositionIds) {
+            if (getNextMoves(+id).length === 0) {
+                endMoves.push(+id)
+                chessBoard.load(pathPositionMap.get(+id).fen)
+            }
+        }
+        trainingPositionIds = trainingPositionIds.filter((id) => {
+            return !endMoves.includes(id)
+        })
     }
+
     function setRandomTrainingPosition() {
-        if (trainingPositionIds.length <= 0){
+        if (trainingPositionIds.length <= 0) {
             return
         }
         const selectedIndex = Math.floor(Math.random() * trainingPositionIds.length)
@@ -70,7 +81,7 @@ function TrainingManager() {
         const selectedId = trainingPositionIds[selectedIndex]
         console.log(`selected id ${selectedId}`)
         setBoardPosition(selectedId)
-        trainingPositionIds.splice(selectedId,1)
+        trainingPositionIds.splice(selectedId, 1)
         console.log(getNextMoves(activePositionId))
         return selectedIndex
     }
@@ -82,6 +93,7 @@ function TrainingManager() {
         }
         return position.fen
     }
+
     function setBoardPosition(id: number) {
         const positionFen = getPathPosition(id)
         if (!positionFen) {
@@ -91,9 +103,11 @@ function TrainingManager() {
         chessBoard.load(positionFen)
         console.log(chessBoard.ascii())
     }
-    function setBoardPositionFen(fen: string){
+
+    function setBoardPositionFen(fen: string) {
         chessBoard.load(fen)
     }
+
     function getNextMoves(id: number) {
         const position = pathPositionMap.get(id)
         if (!position) {
@@ -110,22 +124,33 @@ function TrainingManager() {
         return chessBoard.move({from: sourceSquare, to: targetSquare});
 
     }
+
     function validateNextMove(move: string) {
 
-        if(!getNextMoves(activePositionId).includes(move)){
+        if (!getNextMoves(activePositionId).includes(move)) {
             attemptedMoves++
-            accuracy = Math.round(correctMoves/attemptedMoves*100)
+            accuracy = Math.round(correctMoves / attemptedMoves * 100)
             console.log(`accuracy: ${accuracy}`)
             return false
         }
         correctMoves++
         attemptedMoves++
-        accuracy = Math.round(correctMoves/attemptedMoves*100)
+        accuracy = Math.round(correctMoves / attemptedMoves * 100)
         console.log(`accuracy: ${accuracy}`)
         return true
 
     }
 
-    return {setupTraining, getBoardPosition, setBoardPositionFen, setRandomTrainingPosition, makeMove, validateNextMove, getUserPathways, startOpeningTest}
+    return {
+        setupTraining,
+        getBoardPosition,
+        setBoardPositionFen,
+        setRandomTrainingPosition,
+        makeMove,
+        validateNextMove,
+        getUserPathways,
+        startOpeningTest
+    }
 }
-export{ TrainingManager }
+
+export {TrainingManager}
