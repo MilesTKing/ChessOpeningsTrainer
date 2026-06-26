aa635bc343f7c58fad7c27e1aa8c62665ae57c11
Miles King <kingmilest@gmail.com>
2026-06-26 03:44:11 -0400

    Added palette to page css file. Updated pages styles to reference palette.

--- frontend/src/pages/Openings.tsx
+++ frontend/src/pages/Openings.tsx
@@ -1,14 +1,15 @@
 import { Chessboard } from 'react-chessboard';
 import Header from '../components/nav/Header.tsx'
+import pageStyles from './Page.module.css'
 function Openings(){
     const chessboardOptions = {
         // your config options here
     };
 
-    return <>
+    return <div className={pageStyles.page}>
         <Header></Header>
         <Chessboard options={chessboardOptions} />;
-    </>
+    </div>
 
 }
 export default Openings
--- frontend/src/pages/Training.tsx
+++ frontend/src/pages/Training.tsx
@@ -1,14 +1,16 @@
 import { Chessboard } from 'react-chessboard';
 import Header from '../components/nav/Header.tsx'
+import pageStyles from './Page.module.css'
+
 function Training(){
     const chessboardOptions = {
         // your config options here
     };
 
-    return <>
+    return <div className={pageStyles.page}>
         <Header></Header>
         <Chessboard options={chessboardOptions} />;
-    </>
+    </div>
 
 }
 export default Training;
