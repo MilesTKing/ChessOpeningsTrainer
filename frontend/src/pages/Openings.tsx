c61afd73047743dbc24b96bd3a59e4fde5a3d030
Miles King <kingmilest@gmail.com>
2026-07-09 09:08:06 -0400

    Set gameboard and header height to fit on screen.

--- frontend/src/pages/Openings.tsx
+++ frontend/src/pages/Openings.tsx
@@ -1,21 +1,28 @@
-import { Chessboard } from 'react-chessboard';
+import {Chessboard} from 'react-chessboard';
 import Header from '../components/nav/Header.tsx'
 import Analysis from '../components/gameboard/Analysis.tsx'
 import pageStyles from './Page.module.css'
 import styles from './Openings.module.css'
 
-function Openings(){
+function Openings() {
     const chessboardOptions = {
-        // your config options here
+        boardStyle: {
+            display: "grid",
+            overflow: "hidden",
+            width: '92vh',
+            height: '100%',
+            position: 'relative',
+        }
     };
 
     return <div className={pageStyles.page}>
         <Header></Header>
         <section className={styles.gameBoard}>
-            <Chessboard options={chessboardOptions} />
+            <Chessboard options={chessboardOptions}/>
             <Analysis/>
         </section>
     </div>
 
 }
+
 export default Openings
