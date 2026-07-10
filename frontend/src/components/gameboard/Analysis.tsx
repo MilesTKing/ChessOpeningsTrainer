24e6aa05ac5b6a80a6ea0637abf439a34a6fbbdb
Miles King <kingmilest@gmail.com>
2026-07-09 21:08:06 -0400

    Added event handler to change analysis body on nav bar selection.

--- frontend/src/components/gameboard/Analysis.tsx
+++ frontend/src/components/gameboard/Analysis.tsx
@@ -1,14 +1,29 @@
+import {useState} from 'react'
 import styles from './Analysis.module.css'
 export default function Analysis(){
+    const [analysisDisplay, setAnalysisDisplay] = useState('nodes')
+
+    function handleNavBarSelection(navSelection: string){
+        setAnalysisDisplay(navSelection)
+        return
+    }
+
+    function setAnalysisBody(){
+        switch(analysisDisplay){
+            case 'nodes':
+                return (<div className={styles.analysisBody}>{analysisDisplay}</div>)
+            case 'settings':
+                return (<div className={styles.analysisBody}>{analysisDisplay}</div>)
+        }
+    }
     return (
         <div className={styles.analysis}>
             <h1 className={styles.title}>Opening Path</h1>
             <div className={styles.navBar}>
-                <div className={styles.navBarItem}>Nodes</div>
-                <div className={styles.navBarItem}>Settings</div>
+                <button className={styles.navBarItem} onClick={()=>handleNavBarSelection('nodes')}>Nodes</button>
+                <button className={styles.navBarItem} onClick={()=>handleNavBarSelection('settings')}>Settings</button>
             </div>
-            <div className={styles.nodeContainer}>bear</div>
+            {setAnalysisBody()}
         </div>
-        // settings, nodes, 
     )
 }
